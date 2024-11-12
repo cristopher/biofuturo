0cc054952eb20e779c5c43b6a2dbc92bc6457ec0
Cristopher Castro <hola@cristopher.cl>
2024-11-11 21:42:08 -0300

    updateLocalstorage

---
+++ localdata.js
@@ -0,0 +1,85 @@
+if (storageAvailable('localStorage')) {
+    checkDatabase();
+}
+
+function storageAvailable(type) {
+    var storage;
+    try {
+        storage = window[type];
+        var x = '__storage_test__';
+        storage.setItem(x, x);
+        storage.removeItem(x);
+        return true;
+    }
+    catch(e) {
+        return e instanceof DOMException && (
+            // everything except Firefox
+            e.code === 22 ||
+            // Firefox
+            e.code === 1014 ||
+            // test name field too, because code might not be present
+            // everything except Firefox
+            e.name === 'QuotaExceededError' ||
+            // Firefox
+            e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+            // acknowledge QuotaExceededError only if there's something already stored
+            (storage && storage.length !== 0);
+        }
+}
+
+function checkDatabase() {
+    if (haveDatabase() == true) {
+        checkIntegrity();
+        verifyLogin();
+    } else {
+        makeDatabase();
+    }
+}
+
+function haveDatabase() {
+    if (localStorage.biofuturo != null) {
+        return true;
+    }
+    return false;
+}
+
+function checkIntegrity() {
+    let biofuturo = localStorage["biofuturo"];
+    if (biofuturo){
+        let db = JSON.parse(localStorage["biofuturo"]);
+
+        let tables = ['login'];
+    
+        for (var j = 0; j < tables.length; j++) {
+            let table = false;
+
+            Object.keys(db).forEach(function(key) {
+                if (key == tables[j]) {
+                    table = true;
+                }
+            });
+    
+            if (table == false) {
+                let element = JSON.parse('{"' + tables[j] + '":[]}');
+                db = { ...db, ...element };
+            }
+        }
+    
+        localStorage["biofuturo"] = JSON.stringify(db);
+    }
+}
+
+function makeDatabase() {
+    var db = JSON.parse('{\"login\":false}');
+    localStorage["biofuturo"] = JSON.stringify(db);
+}
+
+function verifyLogin() {
+    let db = JSON.parse(localStorage["biofuturo"]);
+
+    if (db.login == true){
+        the("login").classList.add("d-none")
+        the("inicio").classList.remove("d-none")
+        the("navInicio").classList.remove("d-none")
+    }
+}
--- login.js
+++ login.js
@@ -33,6 +33,12 @@ the("botonLogin").onclick = function(e){
             the("login").classList.add("d-none")
             the("inicio").classList.remove("d-none")
             the("navInicio").classList.remove("d-none")
+
+            let db = JSON.parse(localStorage["biofuturo"]);
+
+            db.login = true
+
+            localStorage["biofuturo"] = JSON.stringify(db);
         }else{
             make.alert('<p class="text-center">No Autorizado</p>', true) 
         }
@@ -40,4 +46,16 @@ the("botonLogin").onclick = function(e){
     }).catch(error => {
         make.alert('<p class="text-center">Problema de internet o servidor</p>', true) 
     })
+}
+
+
+the("botonLogout").onclick = function(e){
+    let db = JSON.parse(localStorage["biofuturo"]);
+
+    db.login = false
+    localStorage["biofuturo"] = JSON.stringify(db);
+
+    the("login").classList.remove("d-none")
+    the("inicio").classList.add("d-none")
+    the("navInicio").classList.add("d-none")
 }
